f5a2bdfda9c085a802fbb6c27fdc74523eb4c3da
Sean Park <pajaho@gmail.com>
2020-11-08 17:37:39 +0900

    미들웨어 추가
    로그인 등 실행전에 기능을 추가할때 사용

--- 03_express/routing/app.js
+++ 03_express/routing/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const logger = require('morgan');
+
 const admin = require('./routes/admin');
 
 const app = express();
@@ -9,12 +11,21 @@ nunjucks.configure('templates', {
     autoescape : true ,  // true : XSS, 태그 등 파싱되지 않도록 처리
     express : app
 });
+ 
+
+// 미들웨어 세팅
+app.use( logger('dev') );
 
 app.get('/', (req,res) => {
     res.send('express start');
 });
 
-app.use('/admin', admin);
+function vipMiddleware( req, res, next){
+    console.log('최우선 미들웨어');
+    next();
+}
+
+app.use('/admin', vipMiddleware, admin);
 
 app.listen( port, () => {
     console.log('Express listening on port', port);
--- 03_express/routing/routes/admin.js
+++ 03_express/routing/routes/admin.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+function testMiddleware( rew, res, next ){
+    console.log('첫번째 미들웨어');
+    next();
+}
+
+function testMiddleware2( rew, res, next ){
+    console.log('두번째 미들웨어');
+    next();
+}
+
+// 로그인 미들웨어 사용시 추가
+function loginRequired( rew, res, next ){
+    if('로그인이 되어 있지 않으면'){
+        res.redirect('로그인창으로')
+    } else{
+        next();
+
+    }
+}
+
+router.get('/', testMiddleware, testMiddleware2, (req, res) => {
     res.send('admin 이후 url');
 });
 
